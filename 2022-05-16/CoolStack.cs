d6140b0a2b43077c45cc1e6ecc42620a519e8407
cunananan <cunananan@gmail.com>
2022-05-18 15:12:58 -0500

    Complete 5/16

--- 2022-05-16/CoolStack.cs
+++ 2022-05-16/CoolStack.cs
@@ -1,4 +1,47 @@
-public class CoolStack<T> where T : IComparable
+public class CoolStack
 {
-    
+    private Stack<int> _stack;
+    private int _min;
+
+    public CoolStack()
+    {
+        _stack = new Stack<int>();
+    }
+
+    public void Push(int n)
+    {
+        if (_stack.Count() <= 0) {
+            _min = n;
+            _stack.Push(n);
+        } else {
+            if (n < _min) {
+                _stack.Push(2*n - _min);
+                _min = n;
+            } else {
+                _stack.Push(n);
+            }
+        }
+    }
+
+    public int? Pop()
+    {
+        if (_stack.Count <= 0) {
+            return null;
+        }
+        int top = _stack.Pop();
+        if (top < _min) {
+            int temp = _min;
+            _min = 2*_min - top;
+            top = temp;
+        }
+        return top;
+    }
+
+    public int? Min()
+    {
+        if (_stack.Count <= 0) {
+            return null;
+        }
+        return _min;
+    }
 }
--- 2022-05-16/Program.cs
+++ 2022-05-16/Program.cs
@@ -29,7 +29,7 @@ string LinkedListToString(LinkedList<int> ll)
     return str;
 }
 
-Console.WriteLine("LinkedListAdder tests:");
+Console.WriteLine("Sum Lists:");
 Console.WriteLine("--Forward--");
 
 LinkedList<int> i0 = IntToLinkedList(5234);
@@ -37,11 +37,11 @@ LinkedList<int> i1 = IntToLinkedList(9068);
 LinkedList<int> i2 = IntToLinkedList(45);
 LinkedList<int> i3 = IntToLinkedList(2058);
 
-// 5234 + 9068 = 14302
+Console.WriteLine("5234 + 9068 = 14302");
 Console.WriteLine(LinkedListToString(i0) + " + " +
                   LinkedListToString(i1) + " = " +
                   LinkedListToString(LinkedListAdder.Add(i0, i1)));
-// 45 + 2058 = 2103
+Console.WriteLine("45 + 2058 = 2103");
 Console.WriteLine(LinkedListToString(i2) + " + " +
                   LinkedListToString(i3) + " = " +
                   LinkedListToString(LinkedListAdder.Add(i2, i3)));
@@ -53,15 +53,36 @@ LinkedList<int> r3 = IntToLinkedList(72, true);
 
 Console.WriteLine("--Reversed--");
 
-// 1234 + 9876 = 11110
+Console.WriteLine("1234 + 9876 = 11110");
 Console.WriteLine(LinkedListToString(r0) + " + " +
                   LinkedListToString(r1) + " = " +
                   LinkedListToString(LinkedListAdder.AddReversed(r0, r1)));
-// 358 + 72 = 430
+Console.WriteLine("358 + 72 = 430");
 Console.WriteLine(LinkedListToString(r2) + " + " +
                   LinkedListToString(r3) + " = " +
                   LinkedListToString(LinkedListAdder.AddReversed(r2, r3)));
 
 
-Console.WriteLine("\nCoolStack tests:");
+Console.WriteLine("\nStack Min:");
 
+CoolStack stack = new CoolStack();
+stack.Push(10);
+stack.Push(16);
+stack.Push(12);
+stack.Push(7);
+stack.Push(8);
+Console.WriteLine("[ 8, 7, 12, 16, 10 ]");
+Console.WriteLine("Min = " + stack.Min());
+stack.Push(5);
+Console.WriteLine("Pushing '5'");
+Console.WriteLine("[ 5, 8, 7, 12, 16, 10 ]");
+Console.WriteLine("Min = " + stack.Min());
+stack.Pop();
+stack.Pop();
+Console.WriteLine("Popping x2");
+Console.WriteLine("[ 7, 12, 16, 10 ]");
+Console.WriteLine("Min = " + stack.Min());
+stack.Pop();
+Console.WriteLine("Popping");
+Console.WriteLine("[ 12, 16, 10 ]");
+Console.WriteLine("Min = " + stack.Min());
